6ac13ca4e9f5173b544a481701d95e68cd588dea
drumnation <davidmieloch@gmail.com>
2017-04-09 22:13:58 -0400

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,29 +1,62 @@
 class OwnersController < ApplicationController
+  def pets
+    @pets = Pet.all
+    # makes an array of all the pet objects available as a method
+  end
 
   get '/owners' do
     @owners = Owner.all
-    erb :'/owners/index' 
+    erb :'/owners/index'
+    # makes an array of all the owner objects
+    # renders the list of owners
   end
 
-  get '/owners/new' do 
+  get '/owners/new' do
     erb :'/owners/new'
+    # renders the new owner form
   end
 
-  post '/owners' do 
-    
+  post '/owners' do
+    @owner = Owner.create(params[:owner])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    @owner.save
+    redirect "/owners/#{ @owner.id }"
+    # create a new owner
+    # if the owner has no pets create one
+    # otherwise save the owner
+    # redirect to owner show page
   end
 
-  get '/owners/:id/edit' do 
+  get '/owners/:id/edit' do
     @owner = Owner.find(params[:id])
     erb :'/owners/edit'
+    # finds existing owner by id and sets instance variable @owner
+    # renders edit owners page
   end
 
-  get '/owners/:id' do 
+  get '/owners/:id' do
     @owner = Owner.find(params[:id])
     erb :'/owners/show'
+    # finds existing owner id and sets instance variable @owner
+    # renders owners show page
   end
 
-  post '/owners/:id' do 
-   
-  end
+  post '/owners/:id' do
+     @owner = Owner.find(params[:id])
+     @owner.update(params["owner"])
+     if !params["pet"]["name"].empty?
+       @owner.pets << Pet.create(name: params["pet"]["name"])
+       @owner.save
+     end
+     redirect to "owners/#{@owner.id}"
+     # finds an existing owner and sets instance variable @owner
+     # updates @owner with form input owner params
+     # if owner has no pets
+     # create new pets with form input params and update owner's pets
+     # save owner
+     # redirect to the owner's unique id page
+   end
+
 end
--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,26 +1,63 @@
 class PetsController < ApplicationController
 
+  def owners
+    @owners = Owner.all
+    # sets a class method providing access to all the owners
+  end
+
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
+    # makes an array of all pet objects available in the @pets variable
+    # renders the index page
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
     erb :'/pets/new'
+    # render the new.erb page
   end
 
-  post '/pets' do 
-
-    redirect to "pets/#{@pet.id}"
+  post '/pets' do
+    @pet = Pet.create(params[:pet])
+    if !params[:owner][:name].empty?
+      @pet.owner = Owner.create(params[:owner])
+    end
+    @pet.save
+    redirect to "/pets/#{ @pet.id }"
+    # create pet
+    # of owner name has no pets
+    # set the pet's owner equal to a new owner object
+    # save the pet
+    # redirect to the pet's page based on it's unique id
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
+    # finds the pet object based on the uri and saves it to the @pet instance variable
+    # renders the /pets/show page
   end
 
-  post '/pets/:id' do 
-
-    redirect to "pets/#{@pet.id}"
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+    # finds the pet object based on the uri and saves it to the @pet instance variable
+    # renders the pets/edit page
   end
+
+  post '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    @pet.update(params[:pet])
+    if !params[:owner][:name].empty?
+      @pet.owner = Owner.create(params[:owner])
+    end
+    @pet.save
+    redirect to "pets/#{ @pet.id }"
+    # find pet by id and set equal to the instance variable @pet
+    # update the db with the new parameters
+    # if owner doesn't exist create a new one and update associate with pet
+    # save @pet
+    # redirect to that pet's unique page id
+  end
+
 end
